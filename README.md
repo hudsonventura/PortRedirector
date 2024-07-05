92b234299340a4639ac649e8fea13058ed7391c4
Hudson Ventura <hudsonventura@outlook.com>
2024-07-05 08:11:06 -0400

    Adding the readme file

---
+++ README.md
@@ -0,0 +1,36 @@
+# RedirectorPort
+
+It's create a way to redirect some ports to diferent destinies and ports. It works like a proxy, but not only HTTP conection, but any other type.    
+
+To put it online, edit the file `appsettigs.json` and add all of your services as much as you need.  
+
+``` json
+{
+    "redirects": [
+        {
+            "title": "test 1",          //it's just a name to show in log
+            "port": 12345,              //it's the port you will conect
+            "destiny": "10.10.1.201:22" //it's the url and port (respect the format 'url:port') that your connection is goint to bem redirected
+        },
+        {
+            "title": "test 2",
+            "port": 12344,
+            "destiny": "10.10.1.201:24"
+        }
+    ]
+}
+```  
+  
+  
+After that, just up the containers:
+``` bash
+sudo docker compose up
+```  
+  
+    
+and, go ahead:
+``` bash
+test 1 -> TCP redirector started on 0.0.0.0:12345, redirecting to 10.10.1.201:22
+test 2 -> TCP redirector started on 0.0.0.0:12344, redirecting to 10.10.1.201:24
+```
+
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+
+IConfiguration appsettings = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var redirectEntries = new List<portredirector.RedirectEntry>();
+appsettings.GetSection("redirects").Bind(redirectEntries);
+
+var tasks = new List<Task>();
+
+foreach (var redirect in redirectEntries)
+{
+    var localIP = IPAddress.Any;
+    var localPort = redirect.port;
+    var remoteIP = IPAddress.Parse(redirect.destiny.Split(":")[0]);
+    var remotePort = int.Parse(redirect.destiny.Split(":")[1]);
+
+    var redirector = new portredirector.TcpPortRedirector(localIP, localPort, remoteIP, remotePort);
+    tasks.Add(redirector.StartAsync());
+
+    Console.WriteLine($"{redirect.title} -> TCP redirector started on {localIP}:{localPort}, redirecting to {remoteIP}:{remotePort}");
+}
+
+await Task.WhenAll(tasks);
